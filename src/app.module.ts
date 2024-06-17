103f617381523f2e15962ef167e04ce70244e7c2
puncharrow5 <osh4761@hanmail.net>
2024-06-17 15:32:24 +0900

    feat: Admin 리소스

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UiModule } from './ui/ui.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [UiModule],
+  imports: [UiModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
