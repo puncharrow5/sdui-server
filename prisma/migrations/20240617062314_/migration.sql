057db255a664255ec9abd34a2b00948bb9575daa
puncharrow5 <osh4761@hanmail.net>
2024-06-17 15:24:37 +0900

    feat: nestjs 환경 세팅

---
+++ prisma/migrations/20240617062314_/migration.sql
@@ -0,0 +1,37 @@
+-- CreateTable
+CREATE TABLE `Site` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `domain` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Admin` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `siteId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Component` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `type` ENUM('Header', 'Popup', 'Section', 'Footer') NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `title` VARCHAR(191) NULL,
+    `content` VARCHAR(191) NULL,
+    `background` VARCHAR(191) NULL,
+    `siteId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Admin` ADD CONSTRAINT `Admin_siteId_fkey` FOREIGN KEY (`siteId`) REFERENCES `Site`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Component` ADD CONSTRAINT `Component_siteId_fkey` FOREIGN KEY (`siteId`) REFERENCES `Site`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,54 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
+import { graphqlUploadExpress } from 'graphql-upload';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+/**
+ * [maxFileSize]
+ * 1) 10진수를 따름
+ * 2) maxFileSize=14일 경우 14미만 조건임
+ * 3) 배열로 파일이 들어오더라도 단일개의 파일 사이즈에 해당함
+ */
+const maxFileSize = 1000 * 1000 * 500;
+const maxFiles = 220;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {});
+
+  app.useStaticAssets(join(__dirname, '..', '..', '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', '..', '..', 'views'));
+  app.setViewEngine('ejs');
+  app.use(cookieParser());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use('/graphql', graphqlUploadExpress({ maxFileSize, maxFiles }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // true일 경우 validation decorator가 붙어있지 않는 속성들은 제거
+      forbidNonWhitelisted: false, // true일 경우 DTO에 존재하지 않는 값이 들어올 경우 에러, 이미지 경우 false로 해야 인식한다.
+      transform: true, // 모든 데이터는 string으로 전달되는데 true일 경우 DTO에 도달할 때 형변환 시켜준다.
+      stopAtFirstError: true, // 에러가 여러개일 경우 첫번째 메세지만 보내게 설정
+    }),
+  );
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('랜딩 페이지 UI 관리 서버')
+      .setDescription('API 명세서')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
