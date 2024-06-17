b5a0cb884ac5c5cebfd0aec3eb367fd5e75d7f7b
puncharrow5 <osh4761@hanmail.net>
2024-06-17 18:11:44 +0900

    chore: prisma 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
 import { ComponentModule } from './component/component.module';
+import { PrismaModule, loggingMiddleware } from 'nestjs-prisma';
+import { SiteModule } from './site/site.module';
 
 @Module({
-  imports: [AdminModule, ComponentModule],
+  imports: [
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        middlewares: [
+          loggingMiddleware({
+            logger: new Logger('PrismaMiddleWare'),
+            logLevel: 'log',
+          }),
+        ],
+      },
+    }),
+    AdminModule,
+    ComponentModule,
+    SiteModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
