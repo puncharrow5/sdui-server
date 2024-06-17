0610a70fa71f7eb7a6ea1ae9da853e1bd2ae7f99
puncharrow5 <osh4761@hanmail.net>
2024-06-17 18:11:57 +0900

    feat: admin 리소스

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAdminInput } from './dto/create-admin.input';
-import { UpdateAdminInput } from './dto/update-admin.input';
 
 @Injectable()
 export class AdminService {
@@ -8,19 +7,7 @@ export class AdminService {
     return 'This action adds a new admin';
   }
 
-  findAll() {
-    return `This action returns all admin`;
-  }
-
   findOne(id: number) {
     return `This action returns a #${id} admin`;
-  }
-
-  update(id: number, updateAdminInput: UpdateAdminInput) {
-    return `This action updates a #${id} admin`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} admin`;
   }
 }
